af2a1db5a005d4990037a885f9ec50fb4bf0ec82
Abu Azis <muhammadazis801@gmail.com>
2020-06-01 13:37:04 +0700

    15-navigasi-multi-screen-flutter

---
+++ lib/main_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/second_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+           child: Text("Go To Second Page"),
+           onPressed: () {
+             Navigator.push(
+              context, 
+              MaterialPageRoute(
+                builder: (context) {
+                  return SecondScreen();
+                }
+              )
+             );
+           },
+        ),
+      ),
+    );
+  }
+}
