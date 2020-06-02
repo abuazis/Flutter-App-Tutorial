d36e33a70a6bd30a7e2c48bee54c1b6304b789e9
Abu Azis <muhammadazis801@gmail.com>
2020-06-02 19:21:51 +0700

    20-media-query-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Text Field"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.lightBlue[50],
-                  filled: true,
-                  icon: Icon(Icons.adb),
-                  suffix: Container(width: 5, height: 5, color: Colors.red),
-                  prefixIcon: Icon(Icons.person),
-                  prefixText: "Name: ",
-                  prefixStyle: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
-                  labelText: "Nama Lengkap",
-                  labelStyle: TextStyle(),
-                  hintText: "your name",
-                  hintStyle: TextStyle(fontSize: 12),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
-                maxLength: 10,
-              ),
-              Text(controller.text)
-            ],
-          ),
-        ),
-      ),
+      home: MainPage()
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Media Query"),
+      ),
+      body: (MediaQuery.of(context).orientation == Orientation.portrait) ? Column(
+        children: generateContainers(),
+      ) :
+      Row (
+        children: generateContainers(),
+      )
+      // body: Container(
+      //   width: MediaQuery.of(context).size.width / 3,
+      //   height: MediaQuery.of(context).size.height / 2,
+      //   color: Colors.red
+      // ),
+    );
+  }
+
+  List<Widget> generateContainers() {
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100
+      ),
+        Container(
+        color: Colors.green,
+        width: 100,
+        height: 100
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100
+      ),
+    ];
   }
 }
