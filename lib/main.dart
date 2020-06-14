ad3ba4b14421943e537368c642129812d4ea241a
Abu Azis <muhammadazis801@gmail.com>
2020-06-14 07:18:12 +0700

    21-inkwell-button-gradasi-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,49 +6,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage()
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Latihan Media Query"),
-      ),
-      body: (MediaQuery.of(context).orientation == Orientation.portrait) ? Column(
-        children: generateContainers(),
-      ) :
-      Row (
-        children: generateContainers(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Membuat Button"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {},
+                color: Colors.amber,
+                shape: StadiumBorder(),
+                child: Text("Raised Button"),
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                elevation: 2,
+                child: Container(
+                  width: 150,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.purple, Colors.pink],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter
+                    ),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          "My Button",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600
+                          ),
+                        )
+                      ),
+                    )
+                  ),
+                ),
+              )
+            ]
+          ),
+        )
       )
-      // body: Container(
-      //   width: MediaQuery.of(context).size.width / 3,
-      //   height: MediaQuery.of(context).size.height / 2,
-      //   color: Colors.red
-      // ),
     );
-  }
-
-  List<Widget> generateContainers() {
-    return <Widget>[
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100
-      ),
-        Container(
-        color: Colors.green,
-        width: 100,
-        height: 100
-      ),
-      Container(
-        color: Colors.blue,
-        width: 100,
-        height: 100
-      ),
-    ];
   }
 }
