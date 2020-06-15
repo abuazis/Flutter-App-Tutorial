645e4fb68fab13f6c4e3cd4e8e4cc7afe7108a71
Abu Azis <muhammadazis801@gmail.com>
2020-06-15 18:00:54 +0700

    28-read-qrcode-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TabBar myTabBar = TabBar(
-      indicator: BoxDecoration(
-        color: Colors.red,
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.purple,
-            width: 5
-          )
-        )
-      ),
-      indicatorColor: Colors.red,
-      tabs: <Widget>[
-        Tab(
-          icon: Icon(Icons.comment),
-          text: "Comments"
-        ),
-        Tab(
-          icon: Icon(Icons.computer),
-          text: "Computer"
-        ),
-      ],
-    );
-
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Contoh Tab Bar"),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-              child: Container(
-                color: Colors.amber,
-                child: myTabBar
-              ),
-            )
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              Center(child: Text("Tab 1")),
-              Center(child: Text("Tab 2")),
-            ],
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            version: 6,
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
+            padding: EdgeInsets.all(30),
+            size: 300,
+            data: "https://www.youtube.com/channel/UC0s92eMIsyR9otmIiY4v8Ww"
           ),
         ),
-      )
+      ),
     );
   }
 }
