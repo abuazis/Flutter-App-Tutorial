de91b3259d0477f005cb6c8fb4c88ec47127e741
Abu Azis <muhammadazis801@gmail.com>
2020-06-15 17:07:30 +0700

    27-styling-tabbar-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,38 +5,47 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+      indicator: BoxDecoration(
+        color: Colors.red,
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.purple,
+            width: 5
+          )
+        )
+      ),
+      indicatorColor: Colors.red,
+      tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.comment),
+          text: "Comments"
+        ),
+        Tab(
+          icon: Icon(Icons.computer),
+          text: "Computer"
+        ),
+      ],
+    );
+
     return MaterialApp(
       home: DefaultTabController(
-        length: 4,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
             title: Text("Contoh Tab Bar"),
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.comment),
-                  text: "Comments"
-                ),
-                Tab(
-                  child: Image(
-                    image: AssetImage("assets/img/cute-pumpkin.png"),
-                  ),
-                ),
-                Tab(
-                  icon: Icon(Icons.computer)
-                ),
-                Tab(
-                  text: "News"
-                ),
-              ],
-            ),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+              child: Container(
+                color: Colors.amber,
+                child: myTabBar
+              ),
+            )
           ),
           body: TabBarView(
             children: <Widget>[
               Center(child: Text("Tab 1")),
               Center(child: Text("Tab 2")),
-              Center(child: Text("Tab 3")),
-              Center(child: Text("Tab 4")),
             ],
           ),
         ),
