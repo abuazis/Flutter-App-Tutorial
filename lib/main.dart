02c9de9365cb7bb370aabbd8764d08e02235d9c7
Abu Azis <muhammadazis801@gmail.com>
2020-06-17 11:15:24 +0700

    32-read-scan-qrcode-camera-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String durasi = "00:00:00";
-  AudioPlayer audioPlayer;
-
-  _MyAppState() {
-    audioPlayer = AudioPlayer();
-    audioPlayer.onAudioPositionChanged.listen((duration) {
-      setState(() {
-        durasi = duration.toString();
-      });
-    });
-  }
-
-  void playSound(String url) async {
-    await audioPlayer.seek(Duration(seconds: 30));
-    await audioPlayer.play(url);
-  }
-
-  void pauseSound() async {
-    await audioPlayer.pause();
-  }
-
-  void stopSound() async {
-    await audioPlayer.stop();
-  }
-
-  void resumeSound() {
-    audioPlayer.resume();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Playing Sound"),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String text = "Hasil QR Scan";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scan")
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(text),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              child: Text("Scan"),
+              onPressed: () async {
+                text = await scanner.scan();
+                setState(() {});
+              },
+            )
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  playSound("https://www.bensound.com/bensound-music/bensound-creativeminds.mp3");
-                },
-                child: Text("Play"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  pauseSound();
-                },
-                child: Text("Pause"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  stopSound();
-                },
-                child: Text("Stop"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  resumeSound();
-                },
-                child: Text("Resume"),
-              ),
-              Text(
-                durasi,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600
-                )
-              )
-            ],
-          ),
-        ),
-      )
+      ),
     );
   }
 }
