e56721fa577a3c96efc9f1a859532541e5491806
Abu Azis <muhammadazis801@gmail.com>
2020-06-21 07:59:49 +0700

    45-doc-comment-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'bloc/color_bloc.dart';
+import 'widgets/user_profile.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final UserProfile profile = UserProfile(
+    name: "Tony Stark",
+    role: "Iron Man",
+    photoURL: null,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider<ColorBloc>(
-        builder: (context) => ColorBloc(),
-        child: MainPage(),
-      ),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
-    return Scaffold(
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            backgroundColor: Colors.amber,
-            onPressed: () {
-              bloc.dispatch(ColorEvent.to_amber);
-            },
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-            backgroundColor: Colors.lightBlue,
-            onPressed: () {
-              bloc.dispatch(ColorEvent.to_ligth_blue);
-            },
-          )
-        ],
-      ),
-      appBar: AppBar(
-        title: Text("BLoC with flutter_bloc"),
-      ),
-      body: Center(
-        child: BlocBuilder<ColorBloc, Color>(
-          builder: (context, currentColor) => AnimatedContainer(
-            width: 100,
-            height: 100,
-            color: currentColor,
-            duration: Duration(milliseconds: 500),
-          ),
-        ),
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text("Doc Comment Example"),
+            backgroundColor: Colors.red[900]),
+        body: Center(child: profile),
       ),
     );
   }
