ea22e12ea15bd007576f8aa871f5e1b63d983923
Abu Azis <muhammadazis801@gmail.com>
2020-07-01 17:54:48 +0700

    65-accessibility-semantig-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'ui/pages/main_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +8,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Semantics(
+          hidden: true,
+          excludeSemantics: true,
+          child: Text("Aplikasi Aksesbilitas"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
+        child: Semantics(
+          onTapHint: "menambahkan bilangan dengan satu.",
+          child: Icon(Icons.plus_one),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Semantics(
+              label:
+                  "Ini adalah bilangan yang akan ditambahkan. Bilangan saat ini adalah",
+              child: Text(
+                number.toString(),
+                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
