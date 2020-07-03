2c771273c86a3c639b249f84d2339c17ca3ed4f7
Abu Azis <muhammadazis801@gmail.com>
2020-07-04 06:59:08 +0700

    75-cloud-firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/services/auth_services.dart';
-import 'package:flutter_app/ui/wrapper.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_app/services/database_services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider.value(
-      value: AuthServices.firebaseUserStream,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Wrapper(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Firestore Demo"),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Add Data"),
+                  onPressed: () {
+                    DatabaseServices.createOrUpdateProduct(
+                      "1",
+                      name: "Masker",
+                      price: 1000000,
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Edit Data"),
+                  onPressed: () {
+                    DatabaseServices.createOrUpdateProduct(
+                      "1",
+                      name: "Masker",
+                      price: 2000000,
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Delete Data"),
+                  onPressed: () {
+                    DatabaseServices.deleteProduct("1");
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Get Data"),
+                  onPressed: () async {
+                    DocumentSnapshot snapshot = await DatabaseServices.getProduct("1");
+                    print(snapshot.data['nama']);
+                    print(snapshot.data['harga']);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
